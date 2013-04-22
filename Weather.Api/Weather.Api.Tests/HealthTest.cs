51cabed72ccf81c7829aad351bc92366e3c43844
p.ciobanu <p.ciobanu@moodmedia.com>
2013-04-22 16:33:32 +0300

    Nunit

---
+++ Weather.Api/Weather.Api.Tests/HealthTest.cs
@@ -0,0 +1,18 @@
+﻿
+
+using NUnit.Framework;
+namespace Weather.Api.Tests
+{
+    [TestFixture]
+    public class HealthTest
+    {
+        [TestCase]
+       public void GetHealth()
+        {
+            var target = new Health();
+            Assert.AreEqual("REST SERVICE IS UP  AND  RUNNING",target.GetHealth());
+        }
+
+      
+    }
+}
